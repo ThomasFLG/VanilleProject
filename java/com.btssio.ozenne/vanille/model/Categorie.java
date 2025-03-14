52dbce4f7a358354b854b6d4582e1210fbb01f90
ThomasFLG <159135867+ThomasFLG@users.noreply.github.com>
2025-03-14 03:56:33 +0100

    Update Categorie.java

--- java/com.btssio.ozenne/vanille/model/Categorie.java
+++ java/com.btssio.ozenne/vanille/model/Categorie.java
@@ -1,39 +1,29 @@
-package com.btssio.ozenne.vanille.model;
+package com.btssio.ozenne.model;
 
-/**
- * Classe représentant une catégorie de produits.
- */
 public class Categorie {
-    private String id;
-    private String nom;
+	 
+	        private String id;
+	        private String libelle;
+			public String getId() {
+				return id;
+			}
+			public void setId(String id) {
+				this.id = id;
+			}
+			public String getLibelle() {
+				return libelle;
+			}
+			public void setLibelle(String libelle) {
+				this.libelle = libelle;
+			}
+			public Categorie(String id, String libelle) {
+				super();
+				this.id = id;
+				this.libelle = libelle;
+			}
 
-    public Categorie(String id, String nom) {
-        this.id = id;
-        this.nom = nom;
-    }
+	        
+	        
+	    
 
-    // Getters et setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-    
-    @Override
-    public String toString() {
-        return "Categorie{" +
-                "id='" + id + '\'' +
-                ", nom='" + nom + '\'' +
-                '}';
-    }
 }
